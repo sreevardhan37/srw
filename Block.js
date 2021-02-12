44def81a19b8edddf1ebcdde14ce703ac52cfc33
sreevardhan37 <74353232+sreevardhan37@users.noreply.github.com>
2021-02-12 03:25:46 -0800

    Add files via upload

--- Block.js
+++ Block.js
@@ -7,12 +7,17 @@ class Block{
             //isStatic:true
            
         }
+        this.visibility = 225;
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
         World.add(world, this.body);
+        
       }
+     
       display(){
+        console.log(this.body.speed);
+        if(this.body.speed <3){
         var angle = this.body.angle;
         var pos= this.body.position;
         push();
@@ -21,5 +26,12 @@ class Block{
         rectMode(CENTER);
         rect(0,0,this.width, this.height);
         pop();
+      }else{
+        World.remove(world, this.body);
+        push();
+        this.visibility = this.visibility -5;
+        pop();
+        
       }
+    }
 }
